d50f3f5fc17dab5f0d6d10f3f8eb88bc1863d74c
LuK1337 <priv.luk@gmail.com>
2017-05-25 20:55:38 +0200

    gts28vewifi: Allow installation on T713XXU2BQD3 and update fingerprint
    Change-Id: Ib3a759c584d59e1870413e2576941e2b73e24aba

--- validus.mk
+++ validus.mk
@@ -16,5 +16,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-samsung
 PRODUCT_BUILD_PROP_OVERRIDES += \
     DEVICE_MAINTAINERS="Adam Ovadia (oadam11)" \
     PRODUCT_NAME=gts28vewifixx \
-    BUILD_FINGERPRINT=samsung/gts28vewifixx/gts28vewifi:7.0/NRD90M/T713XXU2BQCO:user/release-keys \
-    PRIVATE_BUILD_DESC="gts28vewifixx-user 7.0 NRD90M T713XXU2BQCO release-keys"
+    BUILD_FINGERPRINT=samsung/gts28vewifixx/gts28vewifi:7.0/NRD90M/T713XXU2BQD3:user/release-keys \
+    PRIVATE_BUILD_DESC="gts28vewifixx-user 7.0 NRD90M T713XXU2BQD3 release-keys"
