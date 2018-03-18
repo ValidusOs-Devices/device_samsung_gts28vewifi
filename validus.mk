9b6e3688eedadefe53f6672fd823b6104313b307
oadam11 <ovadia.adam@gmail.com>
2018-03-18 15:04:10 -0400

    Vali'ify

---
+++ validus.mk
@@ -0,0 +1,21 @@
+$(call inherit-product, device/samsung/gts28vewifi/full_gts28vewifi.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/validus/config/common_full_tablet_wifionly.mk)
+
+PRODUCT_NAME := validus_gts28vewifi
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_DEVICE := gts28vewifi
+PRODUCT_MODEL := SM-T713
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+# Use the latest approved GMS identifiers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Adam Ovadia (oadam11)" \
+    PRODUCT_NAME=gts28vewifixx \
+    BUILD_FINGERPRINT=samsung/gts28vewifixx/gts28vewifi:7.0/NRD90M/T713XXU2BQD3:user/release-keys \
+    PRIVATE_BUILD_DESC="gts28vewifixx-user 7.0 NRD90M T713XXU2BQD3 release-keys"
+
+ 
